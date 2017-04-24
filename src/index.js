aaadca4b2385dc6e99080fc6c727bf3bae3667cf
sachin <sachin.nicky@gmail.com>
2017-04-24 22:07:40 +0530

    Completed eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import numeral from "numeral";
 import "./index.css";
 
+/* eslint-disable no-console */
+
 const courseValue = numeral(1000).format("$0,0.00");
-debugger;
+// debugger;
 console.log(`I would pay ${courseValue} for this awesome course`);
