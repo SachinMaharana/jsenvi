065366ae0575e13eee9f43cb9c5f92a96b9c49d2
sachin <sachin.nicky@gmail.com>
2017-04-24 23:41:47 +0530

    Redo http calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -28,6 +28,16 @@ app.listen(port, err => {
     : open(`http://localhost:${port}`, console.log(`Running on Port ${port}`));
 });
 
-console.log("I am from the masert");
-console.log("Another log");
-console.log("Hello");
+app.get("/users", function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+    {
+      id: 2,
+      firstName: "Tammy",
+      lastName: "Norton",
+      email: "tnorton@yahoo.com"
+    },
+    { id: 3, firstName: "Tina", lastName: "Lee", email: "lee.tina@hotmail.com" }
+  ]);
+});
